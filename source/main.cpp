b7cefb8019d1aa92ec2a44f8d58b343ea5221f8b
SolarKing <thetennisplayah@gmail.com>
2015-02-22 01:29:50 -0600

    change elseifs to ifs

--- source/main.cpp
+++ source/main.cpp
@@ -15,22 +15,27 @@ int main(int argc, char const *argv[])
   for (int i = 1; i < argc; i++)
   {
     std::cout << argv[i] << std::endl;
+    
     if (argv[i] == std::string("-sizeL1"))
     {
       std::cout << "debug: entered into argument -sizeL1 condition" << std::endl;
     }
-    else if (argv[i] == std::string("-sizeL2")) 
+
+    if (argv[i] == std::string("-sizeL2")) 
     {
       std::cout << "debug: entered into argument -sizeL2 condition" << std::endl;
     }
-    else if (argv[i] == std::string("-type"))
+
+    if (argv[i] == std::string("-type"))
     {
       std::cout << "debug: entered into argument -type condition" << std::endl;
     }
-    else if (argv[i] == std::string("-write-policy"))
+
+    if (argv[i] == std::string("-write-policy"))
     {
       std::cout << "debug: entered into argument -write-policy condition" << std::endl;
     }
+
   }
   return 0;
 }

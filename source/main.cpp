572b2c6a18453b8e5c65ad639703201ab0b9c1c8
SolarKing <thetennisplayah@gmail.com>
2015-02-21 22:52:15 -0600

    init commit

---
+++ source/main.cpp
@@ -0,0 +1,14 @@
+
+/**
+ * This is the main.cpp file for the team project.
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+int main(int argc, char const *argv[])
+{
+  /* code */
+  return 0;
+}

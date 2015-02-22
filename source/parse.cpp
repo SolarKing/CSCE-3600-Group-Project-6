4856093f88e0163cf4f89a0309bba5ed89726480
SolarKing <thetennisplayah@gmail.com>
2015-02-22 01:23:35 -0600

    init commit

---
+++ source/parse.cpp
@@ -0,0 +1,18 @@
+/**
+ * TODO
+ * [ ] implement -sizeL1 xx
+ * [ ] implement -sizeL2 xx
+ * [ ] implement -type direct/full
+ * [ ] implement -write-policy through/back
+ */
+
+#include <iostream>
+#include <string>
+
+#include "parse.h"
+
+int main(int argc, char const *argv[])
+{
+  /* code */
+  return 0;
+}

6c9a0284f8b2cae8f48054704b3569250f41ed92
SolarKing <thetennisplayah@gmail.com>
2015-02-22 01:23:11 -0600

    add argument handling

--- source/main.cpp
+++ source/main.cpp
@@ -1,14 +1,36 @@
 
 /**
- * This is the main.cpp file for the team project.
+ * TODO:
+ * [ ] handle arguments into program flow
  */
 
 #include <iostream>
 #include <cstdlib>
 #include <string>
 
+// try to avoid using namespace std
+
 int main(int argc, char const *argv[])
 {
-  /* code */
+  for (int i = 1; i < argc; i++)
+  {
+    std::cout << argv[i] << std::endl;
+    if (argv[i] == std::string("-sizeL1"))
+    {
+      std::cout << "debug: entered into argument -sizeL1 condition" << std::endl;
+    }
+    else if (argv[i] == std::string("-sizeL2")) 
+    {
+      std::cout << "debug: entered into argument -sizeL2 condition" << std::endl;
+    }
+    else if (argv[i] == std::string("-type"))
+    {
+      std::cout << "debug: entered into argument -type condition" << std::endl;
+    }
+    else if (argv[i] == std::string("-write-policy"))
+    {
+      std::cout << "debug: entered into argument -write-policy condition" << std::endl;
+    }
+  }
   return 0;
 }
